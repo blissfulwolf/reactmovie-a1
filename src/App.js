8223486cd2fce499542301e0f1e2ff769320fad6
Jasper Teo <cashgrid@yahoo.com>
2022-12-29 10:43:46 +0800

    movieform
    Adding the new movies into an array movielist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState } from 'react'
 import './App.css';
+import MovieForm from './components/MovieForm'
+
 
 function App() {
+
+  const [movielist, setMovieList] = useState([]);
+
+  const addMovie = (text) => {
+    let id = 1
+    if (movielist.length > 0) {
+      id = movielist[0].id + 1
+    }
+    // addMovie JSON object with 2 properties
+    let addMovie = { id: id, text: text }
+    
+    // INSERT addMovie to the 
+    // Start(... Spread/Copy) of movielist []
+    let newMovieList = [addMovie, ...movielist]
+    console.log(newMovieList);
+    setMovieList(newMovieList)
+  };
+
   return (
 
-
     <div className="movie-app">
-      <h1>Movie App Assignment</h1>
+      <h1>Movie App Assignment 1</h1>
+      <MovieForm addMovie={addMovie} />
 
     </div>
   );
---
+++ src/components/MovieForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+function MovieForm(props) {
+
+    const [input, setInput] = useState("");
+
+    // prevent the FORM from refreshing the page
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // Pass in the input state to addMovie
+        props.addMovie(input);
+        // resets the Input to empty string
+        setInput("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="movie-form">
+            <input onChange={(e) => setInput(e.target.value)}
+                className="movie-input"
+                placeholder="Please add a movie" />
+            <button type="submit" className="addmovie-button">Add Movie</button>
+
+        </form>
+    )
+}
+
+export default MovieForm
